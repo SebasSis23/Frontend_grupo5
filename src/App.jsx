f9d197f7a0d8d1e58bb368038f1dfe40b1d0b286
Aida Alejadra Pacara Flores <af75781459@gmail.com>
2026-06-18 12:28:44 -0400

    menu admin FRONTEND

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
+import UnidadAdmin from "./pages/UnidadAdmin";
 import './App.css';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       {/* Contenedor inferior dividido en dos columnas */}
       <div className="app-body">
         <Sidebar />
-        <Home />
+        <UnidadAdmin />
       </div>
     </div>
   );
